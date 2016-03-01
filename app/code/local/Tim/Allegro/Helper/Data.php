0d63005b287b8a33f3a82ba926516c2824a2aea1
Vlado <vladmsu@ukr.net>
2016-03-01 11:19:38 +0200

    Tim_Allegro structure

---
+++ app/code/local/Tim/Allegro/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Tim
+ *
+ * @category   Tim
+ * @package    Tim_Allegro
+ * @copyright  Copyright (c) 2016 Tim (http://tim.pl)
+ * @author     Vladislav Verbitskiy <vladomsu@gmail.com>
+ */
+class Tim_Allegro_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
